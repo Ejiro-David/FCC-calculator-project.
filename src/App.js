07c0b5bac2cc3dfb0b837a4e2cf1980da40ee3f1
EJ-David <daviderhabor.de@gmail.com>
2022-09-20 01:45:04 +0100

    static version of app built

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import "./App.css";
 
 function App() {
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log(e.target.innerText)
+  }
+
+
   return (
     <div className="App">
       <div className="calc">
         <div id="display"></div>
-        <div className="buttons-container">
-          <button id="clear">{"<"}</button>
-          <button id="divide">/</button>
-          <button id="multiply">*</button>
-          <button id="subtract">-</button>
-          <button id="add">+</button>
-          <button id="equals">=</button>
-          <button id="decimal">.</button>
-          <button id="one">1</button>
-          <button id="two">2</button>
-          <button id="three">3</button>
-          <button id="four">4</button>
-          <button id="five">5</button>
-          <button id="six">6</button>
-          <button id="seven">7</button>
-          <button id="eight">8</button>
-          <button id="nine">9</button>
-          <button id="zero">0</button>
-        </div>
+        <form className="buttons-container">
+          <button id="clear"  onClick={handleClick}>clear</button>
+          <button id="divide"  onClick={handleClick}>/</button>
+          <button id="multiply"  onClick={handleClick}>*</button>
+          <button id="subtract"  onClick={handleClick}>-</button>
+          <button id="add"  onClick={handleClick}>+</button>
+          <button id="equals"  onClick={handleClick}>=</button>
+          <button id="decimal"  onClick={handleClick}>.</button>
+          <button id="one"  onClick={handleClick}>1</button>
+          <button id="two"  onClick={handleClick}>2</button>
+          <button id="three"  onClick={handleClick}>3</button>
+          <button id="four"  onClick={handleClick}>4</button>
+          <button id="five"  onClick={handleClick}>5</button>
+          <button id="six"  onClick={handleClick}>6</button>
+          <button id="seven"  onClick={handleClick}>7</button>
+          <button id="eight"  onClick={handleClick}>8</button>
+          <button id="nine"  onClick={handleClick}>9</button>
+          <button id="zero"  onClick={handleClick}>0</button>
+        </form>
       </div>
     </div>
   );
 }
+
 
 export default App;
