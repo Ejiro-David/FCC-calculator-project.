2c4bbbeefcdcd9b3f358387d838a15adc1992593
EJ-David <daviderhabor.de@gmail.com>
2022-09-21 01:29:49 +0100

    bvjkmnhj

--- src/App.js
+++ src/App.js
@@ -5,31 +5,35 @@ function App() {
   const [display, setDisplay] = useState(0);
   const [operand1, setOperand1] = useState(null);
   const [operand2, setOperand2] = useState(null);
-  const [opr, setOpr] = useState(null);
-  const [answer, setAnswer] = useState(null)
+  const [operator, setOperator] = useState(null);
+  const [answer, setAnswer] = useState(null);
 
   const handleNumPress = (e) => {
     e.preventDefault();
     let text = e.target.innerText;
     let textClass = e.target.className;
-    
-    if(textClass === 'num' && display === 0 && text !== '0'){
-      setDisplay(text)
-    }else if(textClass === 'decimal' && display === 0){
-      setDisplay('0.')
-    }else if(display.split('').includes('.') && textClass === 'num'){
-      setDisplay(display + text)
-    }else if(!display.split('').includes('.') && display !== 0){
-      setDisplay(display + text)
-    }
 
-  }
+    if (textClass === "num" && display === 0 && text !== "0") {
+      setDisplay(text);
+    } else if (textClass === "decimal" && display === 0) {
+      setDisplay("0.");
+    } else if (display.split("").includes(".") && textClass === "num") {
+      setDisplay(display + text);
+    } else if (!display.split("").includes(".") && display !== 0) {
+      setDisplay(display + text);
+    }
+  };
 
   const handleOperator = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     let sign = e.target.innerText;
-
-  
+    if (display !== "0" && operand1 === null) {
+      setOperand1(display);
+      setOperator(sign);
+      setDisplay(display + sign);
+    }else if(){
+      
+    }
   };
 
   const handleCalc = (e) => {
@@ -39,6 +43,10 @@ function App() {
   const handleClear = (e) => {
     e.preventDefault();
     setDisplay(0);
+    setOperand1(null);
+    setOperand2(null);
+    setAnswer(null);
+    setOperator(null);
   };
 
   return (
@@ -55,7 +63,11 @@ function App() {
           <button id="multiply" className="operator" onClick={handleOperator}>
             *
           </button>
-          <button id="subtract" className="operator" onClick={handleOperator}>
+          <button
+            id="subtract"
+            className="operator negative"
+            onClick={handleOperator}
+          >
             -
           </button>
           <button id="add" className="operator" onClick={handleOperator}>
@@ -64,37 +76,37 @@ function App() {
           <button id="equals" className="equals" onClick={handleCalc}>
             =
           </button>
-          <button id="decimal" className="decimal"  onClick={handleNumPress}>
+          <button id="decimal" className="decimal" onClick={handleNumPress}>
             .
           </button>
-          <button id="one" className="num"  onClick={handleNumPress}>
+          <button id="one" className="num" onClick={handleNumPress}>
             1
           </button>
-          <button id="two"  className="num" onClick={handleNumPress}>
+          <button id="two" className="num" onClick={handleNumPress}>
             2
           </button>
-          <button id="three"  className="num" onClick={handleNumPress}>
+          <button id="three" className="num" onClick={handleNumPress}>
             3
           </button>
-          <button id="four" className="num"  onClick={handleNumPress}>
+          <button id="four" className="num" onClick={handleNumPress}>
             4
           </button>
-          <button id="five"  className="num" onClick={handleNumPress}>
+          <button id="five" className="num" onClick={handleNumPress}>
             5
           </button>
-          <button id="six"  className="num" onClick={handleNumPress}>
+          <button id="six" className="num" onClick={handleNumPress}>
             6
           </button>
-          <button id="seven"  className="num" onClick={handleNumPress}>
+          <button id="seven" className="num" onClick={handleNumPress}>
             7
           </button>
-          <button id="eight"  className="num" onClick={handleNumPress}>
+          <button id="eight" className="num" onClick={handleNumPress}>
             8
           </button>
-          <button id="nine"  className="num" onClick={handleNumPress}>
+          <button id="nine" className="num" onClick={handleNumPress}>
             9
           </button>
-          <button id="zero"  className="num" onClick={handleNumPress}>
+          <button id="zero" className="num" onClick={handleNumPress}>
             0
           </button>
         </form>
