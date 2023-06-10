afe84b8543729714e339681f0ee6de414fcd297b
EJ-David <daviderhabor.de@gmail.com>
2023-06-10 21:30:58 +0100

    took a crack at the operator logic and state management

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import "./App.css";
 
 //create reducer for diff types: append, calc, del,
 
-
 //refactor to use redux after you actually learn redux
 // const ACTIONS = {
 //   SET_OPERAND_OPERATOR: "set-operand-operator",
-//   SET_NEXTOPERAND_CALC: "set-nextoperand-calc",
+//   SET_term2_CALC: "set-term2-calc",
 //   CLEAR: 'clear'
 // };
 
 // function reducer(state, { type, payload }) {
 //   switch (type) {
 //     case ACTIONS.SET_OPERAND_OPERATOR:
-//       return { ...state, nextOperand: state.nextOperand + payload };
-//     case ACTIONS.SET_NEXTOPERAND_CALC:
+//       return { ...state, term2: state.term2 + payload };
+//     case ACTIONS.SET_term2_CALC:
 //       break;
 //     case ACTIONS.CLEAR:
 //       break;
@@ -27,121 +26,74 @@ import "./App.css";
 // IMPLEMENT 'CLEAR'
 
 function App() {
-
-
   // checks calc state and processes neccessary oprs
   // const [calcState, dispatch] = useReducer(reducer, initState);
 
-
   //constrols display
-  const [display, setDisplay] = useState('0');
-  const [operand, setOperand] = useState('0')
-  const [operator, setOperator] = useState('0')
-  const [nextOperand, setNextOperand] = useState('0')
-  const [result, setResult] = useState('0')
+  const [display, setDisplay] = useState("0");
+  const [term1, setTerm1] = useState("0");
+  const [operator, setOperator] = useState();
+  const [term2, setTerm2] = useState('');
+  const [result, setResult] = useState('');
 
   const handleClick = (e) => {
     // prevents refresh on button press
     e.preventDefault();
     let text = e.target.innerHTML.toString();
     let textClass = e.target.className;
-    let textId = e.target.id
+    let textId = e.target.id;
 
     if (text === "clear") {
-      setDisplay('0');
-      setNextOperand('0')
-      setOperand('0')
-      setOperator('0')
-    } else if(textClass === 'num') {
+      setTerm2();
+      setTerm1('0');
+      setOperator();
+      setDisplay("0");
+    } else if (textClass === "num") {
       numberValidator(text, textId);
-    } else if(textClass === 'operator') {
-      solver(text, textId)
+    } else if (textClass === "operator") {
+      solver(text, textId);
     }
   };
 
-
-
-
-const solver = (text, id) => {
-  console.log(text)
-  console.log(display)
-  if(display === '0'){
-
-  }else{
-    if(nextOperand === '0'){
-      console.log('check 1')
-      setOperand(display)
+  const solver = (text, id) => {
+    // console.log(text, id);
+    setTerm1(display)
+    if(term2 === ''){
       setOperator(text)
-      setDisplay(operand + operator)
-    }else{
-      console.log('check 2')
-      setResult(eval(setOperand, setOperator, nextOperand))
-      setOperand(result)
-      setDisplay(result)
-      setResult()
-      setNextOperand()
+      setDisplay(display + text)
+      // console.log(term1, operator, term2) BUG: state does not update to reflect new term1
     }
-    
-  }
-}
 
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // -a sign or clear terminates funtion call/validation
-
-  // all console logs are tests.
+  
   // this functions makes sure the integer inputted can be operated , i.e is calculable.
   const numberValidator = (text, id) => {
     // text and kind, number validator recieves inputed text and its class
     // console.log('display is a  ',typeof display, 'text is a  ',typeof text)
     // console.log(display, ' vs ',  text)
     // decimal point validator, to make sure not more than one decimal per integer
-    if(text === '.' && display.includes('.') === false){
-      setDisplay(display + text)
+    if (text === "." && display.includes(".") === false) {
+      setDisplay(display + text);
     }
 
     // removes trailing zeros at the beginning of int and replaces it with inputed num
-    else if('0' === display){
-      if(text === '0'){
-        setDisplay(text)
-      }else if(text !== '0' && id !== 'decimal'){
-        setDisplay(text)
+    else if ("0" === display) {
+      if (text === "0") {
+        setDisplay(text);
+      } else if (text !== "0" && id !== "decimal") {
+        setDisplay(text);
       }
 
-    // appends inputted num
-    }else if(display !== '0'){
-      if(id !== 'decimal'){
-        setDisplay(display + text)
+      // appends inputted num
+    } else if (display !== "0") {
+      if (id !== "decimal") {
+        setDisplay(display + text);
       }
-    }
-    else{
-
+    } else {
     }
   };
-  // console.log(display);
-  // console.log(display.toString().includes("."));
+
   return (
     <div className="App">
       <div className="calc">
@@ -158,11 +110,7 @@ const solver = (text, id) => {
           <button id="multiply" className="operator" onClick={handleClick}>
             *
           </button>
-          <button
-            id="subtract"
-            className="operator"
-            onClick={handleClick}
-          >
+          <button id="subtract" className="operator" onClick={handleClick}>
             -
           </button>
           <button id="add" className="operator" onClick={handleClick}>
